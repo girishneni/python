0179005e80a320cce72b60111ebb2dcf05069067
Sasidhar Gogulapati <sasidhar.g@ramyamlab.com>
2016-12-30 08:03:03 +0530

    lists , operations

---
+++ Lists.py
@@ -0,0 +1,401 @@
+
+# coding: utf-8
+
+# In[1]:
+
+'hi'
+
+
+# In[2]:
+
+# Lists
+
+
+# In[3]:
+
+list = [1,2,3]
+
+
+# In[4]:
+
+list
+
+
+# In[5]:
+
+list =['hi', 7, 3, 'o']
+
+
+# In[6]:
+
+list
+
+
+# In[7]:
+
+len(list)
+
+
+# In[8]:
+
+list[0]
+
+
+# In[9]:
+
+list[3]
+
+
+# In[10]:
+
+#slicing
+
+
+# list[1:]
+
+# In[11]:
+
+list[1:]
+
+
+# In[12]:
+
+list
+
+
+# In[13]:
+
+list[:2]
+
+
+# In[14]:
+
+list + ['new object']
+
+
+# In[15]:
+
+list
+
+
+# In[16]:
+
+list = list + ['new object']
+
+
+# In[17]:
+
+list
+
+
+# In[18]:
+
+list * 2
+
+
+# In[19]:
+
+list * 3
+
+
+# In[20]:
+
+# list methods
+
+
+# In[21]:
+
+newList = [1,2,3]
+
+
+# In[22]:
+
+newList.append('4')
+
+
+# In[23]:
+
+newList
+
+
+# In[24]:
+
+newList.append(5)
+
+
+# In[25]:
+
+newList
+
+
+# In[26]:
+
+#pop
+
+
+# In[27]:
+
+newList.pop(0)
+
+
+# In[ ]:
+
+
+
+
+# In[28]:
+
+newList
+
+
+# In[29]:
+
+l = newList.pop()
+
+
+# In[30]:
+
+l
+
+
+# In[31]:
+
+l
+
+
+# In[32]:
+
+newList
+
+
+# In[33]:
+
+newList[0]
+
+
+# In[34]:
+
+newList[100]
+
+strList = ['a', 'b', 'c', 'd']
+# In[35]:
+
+strList
+
+
+# In[36]:
+
+strList = ['a', 'b', 'c', 'd']
+
+
+# In[37]:
+
+strList
+
+
+# In[38]:
+
+strList.reverse()
+
+
+# In[39]:
+
+strList
+
+
+# In[40]:
+
+strList.sort()
+
+
+# In[41]:
+
+strList
+
+
+# In[42]:
+
+#Nested Lists
+
+
+# In[43]:
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+list3 = [7,8,9]
+
+
+# In[44]:
+
+list3
+
+
+# In[45]:
+
+#create nested list
+
+nestedList = [list1, list2, list3]
+# In[46]:
+
+nestedList
+
+
+# In[47]:
+
+nestedList = [list1, list2, list3]
+
+
+# In[48]:
+
+nestedList
+
+
+# In[49]:
+
+nested[0][0]
+
+
+# In[50]:
+
+nestedList[0][0]
+
+
+# In[51]:
+
+nestedList
+
+
+# In[52]:
+
+nestedList[0][2]
+
+
+# In[53]:
+
+nestedList[0][2]
+
+
+# In[54]:
+
+nestedList
+
+
+# In[55]:
+
+nestedList.pop()
+
+
+# In[56]:
+
+nestedList
+
+
+# In[57]:
+
+nestedList.count()
+
+
+# In[58]:
+
+nestedList.count(0)
+
+
+# In[59]:
+
+nestedList.count(2)
+
+
+# In[60]:
+
+list = [1,2,3]
+
+
+# In[61]:
+
+list.count(1)
+
+
+# In[62]:
+
+list = [1,2,3, 1,2,2]
+
+
+# In[63]:
+
+list.count(2)
+
+
+# In[64]:
+
+list.append([4,5])
+
+
+# In[65]:
+
+list
+
+
+# In[66]:
+
+list.count(4)
+
+
+# In[67]:
+
+list.insert(2, 'inserted element')
+
+
+# In[68]:
+
+list
+
+
+# In[69]:
+
+list.remove(2)
+
+
+# In[70]:
+
+list
+
+
+# In[71]:
+
+list.remove(2)
+
+
+# In[72]:
+
+list
+
+
+# In[73]:
+
+list.remove('inserted element')
+
+
+# In[74]:
+
+list
+
+
+# In[75]:
+
+list(3)
+
+list
+# In[77]:
+
+list.remove(3)
+
+
+# In[78]:
+
+list
+
+
+# In[ ]:
+
+
+
